7b22cb13476c2f144ef4832fd3826d350c86dbac
Simon Tse minix.xie@gmail.com <simon.tse@lalamove.com>
2017-08-15 00:48:33 +0800

    check duplicate when signup

--- endpoints/Signup.go
+++ endpoints/Signup.go
@@ -18,7 +18,8 @@ func Signup(userDB *sql.DB) func(ctx echo.Context) error {
         Password string         `json:"password"`
     }
     type ResponseJson struct {
-        userId int64             `json:"userId"`
+        Error string             `json:"error"`
+        UserId int64             `json:"userId"`
         SignupAt int64           `json:"signupAt"`
         SignupAtISO string       `json:"signupAtISO"`
         Jwt string               `json:"jwt"`
@@ -30,76 +31,79 @@ func Signup(userDB *sql.DB) func(ctx echo.Context) error {
         }
         log.Printf("req.Phone = %s", req.Phone)
 
-        nowNano := time.Now().UnixNano()
-        nowMicro := nowNano / 1000
-        nowISO := time.Unix(0, nowNano).UTC().Format(time.RFC3339Nano)
-        user := entity.User{
-            Phone: req.Phone,
-            Password: req.Password,
-            CreatedAtMicroseconds: nowMicro,
-            CreatedAtISO: nowISO,
-            UpdatedAtMicroseconds: nowMicro,
-            UpdatedAtISO: nowISO,
-        }
-
-        stmt, err := userDB.Prepare(`
-            INSERT INTO "User" ("phone", "password", "createdAtMicroseconds", "updatedAtMicroseconds") 
-            VALUES ($1, $2, $3, $4) 
-            RETURNING id
+        // (1) check if phone duplicates
+        stmt1, err := userDB.Prepare(`
+            SELECT count(1)
+            FROM
+            "User"
+            WHERE "phone" = $1
+            LIMIT 1
         `)
-        defer stmt.Close()
+        defer stmt1.Close()
         if err != nil {
             log.Fatal("Cannot prepare DB statement: ", err)
         }
-
-log.Printf("to insert: user = %v", user)
-log.Printf("to insert, phone = \"%s\"", user.Phone)
-log.Printf("to insert, Password = \"%s\"", user.Password)
-
-        var id int64
-        err = stmt.QueryRow(user.Phone, user.Password, user.CreatedAtMicroseconds, user.UpdatedAtMicroseconds).Scan(&id)
+        row := stmt1.QueryRow(req.Phone)
+        var duplicateCount int64
+        err = row.Scan(&duplicateCount)
         if err != nil {
-            log.Fatal("Cannot run insert statement: ", err)
+            log.Fatal("row.Scan has error: ", err)
+        }
+        log.Printf("row count ==> %d", duplicateCount)
+        if duplicateCount > 0 {
+            return ctx.JSON(http.StatusConflict, ResponseJson{
+                Error: "PHONE_ALREADY_REGISTERED",
+            })
         } else {
-            user.Id = id
+            // (2) create user if no duplicate
+            nowNano := time.Now().UnixNano()
+            nowMicro := nowNano / 1000
+            nowISO := time.Unix(0, nowNano).UTC().Format(time.RFC3339Nano)
+            user := entity.User{
+                Phone: req.Phone,
+                Password: req.Password,
+                CreatedAtMicroseconds: nowMicro,
+                CreatedAtISO: nowISO,
+                UpdatedAtMicroseconds: nowMicro,
+                UpdatedAtISO: nowISO,
+            }
+
+            stmt2, err := userDB.Prepare(`
+                INSERT INTO "User" ("phone", "password", "createdAtMicroseconds", "updatedAtMicroseconds") 
+                VALUES ($1, $2, $3, $4) 
+                RETURNING id
+            `)
+            defer stmt2.Close()
+            if err != nil {
+                log.Fatal("Cannot prepare DB statement: ", err)
+            }
+
+    log.Printf("to insert: user = %v", user)
+    log.Printf("to insert, phone = \"%s\"", user.Phone)
+    log.Printf("to insert, Password = \"%s\"", user.Password)
+
+            var id int64
+            err = stmt2.QueryRow(user.Phone, user.Password, user.CreatedAtMicroseconds, user.UpdatedAtMicroseconds).Scan(&id)
+            if err != nil {
+                log.Fatal("Cannot run insert statement: ", err)
+            } else {
+                user.Id = id
+            }
+
+            jwt := lib.GenJWT(user.Id)
+
+            return ctx.JSON(http.StatusOK, ResponseJson{
+                UserId: user.Id,
+                SignupAt: user.CreatedAtMicroseconds,
+                SignupAtISO: user.CreatedAtISO,
+                Jwt: jwt,
+            })
         }
 
-        jwt := lib.GenJWT(user.Id)
 
-        return ctx.JSON(http.StatusOK, ResponseJson{
-            userId: user.Id,
-            SignupAt: user.CreatedAtMicroseconds,
-            SignupAtISO: user.CreatedAtISO,
-            Jwt: jwt,
-        })
 
-        // if req.Phone == "+85267633535" && req.Password == "hello123" {
-        //     nowNano := time.Now().UnixNano()
-        //     nowMicro := nowNano / 1000
-        //     nowISO := time.Unix(0, nowNano).UTC().Format(time.RFC3339Nano)
 
-        //     jwtToken := jwt.New(jwt.SigningMethodHS256)
 
-        //     // Set claims
-        //     claims := jwtToken.Claims.(jwt.MapClaims)
-        //     claims["userId"] = "1234"
-        //     // claims["admin"] = true
-        //     claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
-        //     // Generate encoded token and send it as response.
-
-        //     t, err := jwtToken.SignedString([]byte("secret"))
-        //     if err != nil {
-        //         return err
-        //     }
-
-        //     return ctx.JSON(http.StatusOK, ResponseJson{
-        //         SignupAt: now,
-        //         SignupAtISO: nowISO,
-        //         JwtToken: t,
-        //     })
-        // } else {
-        //     return echo.ErrUnauthorized
-        // }
     }
 }
 
